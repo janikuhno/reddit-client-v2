1cfd1c60fb16635c59b8bd0a155f19da4aacb57e
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-07 22:42:15 +0200

    Cleaning, added dateCalculator

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
+import Card from '../../components/Card';
+import { dateCalculator } from '../../utils/dateCalculator';
 import './Post.css';
 
 const Post = (props) => {
-    const { post } = props;
+  const { post } = props;
 
-    return (
-        <article key={post.id}>
-            <div className="post-wrapper">
-                <div className="post-container">
-                    <h3 className="post-title">{post.title}</h3>
+  return (
+    <article key={post.id}>
+      <Card>
+        <div className="post-wrapper">
+          <div className="post-container">
+            <h3 className="post-title">{post.title}</h3>
 
-                    <div className="post-image-container">
-                        <img src={post.url} alt="" className="post-image" />
-                    </div>
-
-                    <div className="post-details">
-                        <span className="author-details">
-                            <span className="author-username">{post.author}</span>
-                        </span>
-                    </div>
-                </div>
+            <div className="post-image-container">
+              <img src={post.url} alt="" className="post-image" />
             </div>
-        </article>
-    )
+
+            <div className="post-details">
+              <span className="author-details">
+                <span className="author-username">{post.author}</span>
+              </span>
+              <span>{dateCalculator(post.created_utc)}</span>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </article>
+  );
 };
 
 export default Post;
