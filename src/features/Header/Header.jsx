1a43fd27b1f80d7e5d2c64bbe66728552bf010a2
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-08 23:35:52 +0200

    Added search local posts

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HiOutlineSearch } from 'react-icons/hi';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSearchTerm } from '../../store/redditSlice';
 
 const Header = () => {
+  const [searchTermLocal, setSearchTermLocal] = useState('');
+  const searchTerm = useSelector((state) => state.reddit.searchTerm);
+  const dispatch = useDispatch();
+
+  const onSearchTermChange = (e) => {
+    setSearchTermLocal(e.target.value);
+  };
+
+  useEffect(() => {
+    setSearchTermLocal(searchTerm);
+  }, [searchTerm]);
+
+  const onSearchTermSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setSearchTerm(searchTermLocal));
+  };
+
   return (
     <header>
       <div className="logo">
@@ -13,9 +32,15 @@ const Header = () => {
           </p>
         </Link>
       </div>
-      <form className="search">
-        <input type="text" placeholder="Search" aria-label="Search posts" />
-        <button type="submit" aria-label="Search">
+      <form className="search" onSubmit={onSearchTermSubmit}>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTermLocal}
+          onChange={onSearchTermChange}
+          aria-label="Search posts"
+        />
+        <button type="submit" onClick={onSearchTermSubmit} aria-label="Search">
           <HiOutlineSearch />
         </button>
       </form>
