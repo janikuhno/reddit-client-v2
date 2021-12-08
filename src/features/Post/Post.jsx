c439be72a322a5c8b885547581d7d8b4f225611e
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-08 23:51:39 +0200

    Added vote counter

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedPost } from '../../store/redditSlice';
 import Card from '../../components/Card';
 import { dateCalculator } from '../../utils/dateCalculator';
 import './Post.css';
 import Comment from '../Comment/Comment';
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import { TiMessage } from 'react-icons/ti';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import {
+  TiArrowUpOutline,
+  TiArrowUpThick,
+  TiArrowDownOutline,
+  TiArrowDownThick,
+  TiMessage,
+} from 'react-icons/ti';
 import shortenNumber from '../../utils/shortenNumber';
 
 const Post = (props) => {
@@ -15,6 +21,45 @@ const Post = (props) => {
   const dispatch = useDispatch();
   const reddit = useSelector((state) => state.reddit);
   const { selectedPost } = reddit;
+  const [voteValue, setVoteValue] = useState(0);
+
+  /**
+   * @param {number} newValue The new vote value
+   */
+  const onHandleVote = (newValue) => {
+    if (newValue === voteValue) {
+      setVoteValue(0);
+    } else if (newValue === 1) {
+      setVoteValue(1);
+    } else {
+      setVoteValue(-1);
+    }
+  };
+
+  const renderUpVote = () => {
+    if (voteValue === 1) {
+      return <TiArrowUpThick className="icon-action" />;
+    }
+    return <TiArrowUpOutline className="icon-action" />;
+  };
+
+  const renderDownVote = () => {
+    if (voteValue === -1) {
+      return <TiArrowDownThick className="icon-action" />;
+    }
+    return <TiArrowDownOutline className="icon-action" />;
+  };
+
+  const getVoteType = () => {
+    if (voteValue === 1) {
+      return 'up-vote';
+    }
+    if (voteValue === -1) {
+      return 'down-vote';
+    }
+
+    return '';
+  };
 
   const renderComments = () => {
     if (post.errorComments) {
@@ -51,7 +96,9 @@ const Post = (props) => {
     postTitle = (
       <h3
         className="post-title"
-        onClick={() => dispatch(setSelectedPost(post.name), onToggleComments(post.permalink))}
+        onClick={() =>
+          dispatch(setSelectedPost(post.name), onToggleComments(post.permalink))
+        }
       >
         {post.title}
       </h3>
@@ -64,6 +111,31 @@ const Post = (props) => {
     <article key={post.id}>
       <Card>
         <div className="post-wrapper">
+          <div className="post-votes-container">
+            <button
+              type="button"
+              className={`icon-action-button up-vote ${
+                voteValue === 1 && 'active'
+              }`}
+              onClick={() => onHandleVote(1)}
+              aria-label="Up-vote"
+            >
+              {renderUpVote()}
+            </button>
+            <p className={`post-votes-value ${getVoteType()}`}>
+              {shortenNumber(post.ups, 1)}
+            </p>
+            <button
+              type="button"
+              className={`icon-action-button down-vote ${
+                voteValue === -1 && 'active'
+              }`}
+              onClick={() => onHandleVote(-1)}
+              aria-label="Down vote"
+            >
+              {renderDownVote()}
+            </button>
+          </div>
           <div className="post-container">
             {postTitle}
 
