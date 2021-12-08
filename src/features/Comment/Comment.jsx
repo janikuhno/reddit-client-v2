9caf86cd1d8caf9b513677e6561705586be0ecc5
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-08 18:24:33 +0200

    Added Comment, ReactMarkdown module

---
+++ src/features/Comment/Comment.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { dateCalculator } from '../../utils/dateCalculator';
+import './Comment.css';
+
+const Comment = (props) => {
+  const { comment } = props;
+  return (
+    <div className="comment">
+      <div className="comment-metadata">
+        <p className="comment-author">{comment.author}</p>
+        <p className="comment-created-time">
+          {dateCalculator(comment.created_utc)}
+        </p>
+      </div>
+      <ReactMarkdown source={comment.body} />
+    </div>
+  );
+};
+
+export default Comment;
